dbe7844b1fb2f4bf9fe0cf358258ad4d52750ef0
zapataramil <rzapata@fi.uba.ar>
2020-09-21 19:26:00 -0300

    creacion de la recursividad de factorial

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
+int factorial(int numero);
+
 int main(int argc, char** argv){
+	int num;
+	cout <<"numero: "<<endl;
+	cin >> num;
+	cout << "el factorial es: "<<factorial(num);
+
 	return 0;
 }
 
+
+int factorial(int numero){
+	if ((numero==0)||(numero==1))
+		return 1;
+	return (numero)*factorial(numero-1);
+
+}
