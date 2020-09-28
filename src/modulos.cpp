52050603d5bf0083fe6799464dad91becd0d511d
zapataramil <rzapata@fi.uba.ar>
2020-09-27 21:00:41 -0300

    modificacion de PRE y POST condiciones

--- src/modulos.cpp
+++ src/modulos.cpp
@@ -1,14 +1,9 @@
 #include "modulos.h"
 
 
-//RECURSIVIDAD MULTIPLE: Dentro de la funcion se lo llama a la funcion recursiva
 int towerOfHanoi(int number){
 	return 0;
 }
-
-
-
-//RECURSIVIDAD ANIDADA: funcion de ackerman (tarda un montonasooo, crece muy rapido)
 
 int ackermanFunction(int m, int n){
 	if (m==0)
@@ -17,12 +12,6 @@ int ackermanFunction(int m, int n){
 		return ackermanFunction(m-1,1);
 	return ackermanFunction(m-1,ackermanFunction(m,n-1));
 }
-
-
-
-
-
-//MUTUAL RECURSION: example of fibonacci
 
 int fibo (int num){
 	return adult(num)+baby(num);
@@ -40,18 +29,12 @@ int baby(int n){
 	return adult(n-1);
 }
 
-
-//fibonacci implementado con recurisvidad  multiple, return de 2 veces el llamado de recursion
-//
 int fibonacci(int num){
 	if((num==1)||(num==2))
 		return 1;
 	return fibonacci(num-1)+fibonacci(num-2);
 }
 
-
-//factorial1 y factorialRecursivoDeCola ambos juntos funcionan como un factorial recursivo pero de cola, no simple(que usa una pila para almacenar valores en varios niveles e ir multiplicando los resultados)
-//es recursividad de cola, parecieria mutua pero no lo es(porque no hacen ping pong con los parametros, solo una vez al principio)
 int factorial1(int num){
 	return factorialRecursivoDeCola(num,1);
 }
@@ -62,8 +45,6 @@ int factorialRecursivoDeCola(int num,int acum){
 	return factorialRecursivoDeCola(num-1,acum*num);
 }
 
-
-//SIEMPRE DEVUELVO (RETURN) LA FUNCION SIN NINGUNA CUENTA ENCIMA...ES RECURSION DE COLA, SIEMPRE TRABAJO SOBRE EL MISMO NIVEL(ES MEJOR)
 int MCD(int a, int b){
 	if((a==0)||(b==0))
 		return 0;
@@ -76,7 +57,6 @@ int MCD(int a, int b){
 	}
 }
 
-//AL IGUAL QUE POTENCIA Y FACTORIAL, VOY ACUMULANDO DATOS EN LA PILA...EL RETURN HACE SUMAS ENTRE LA LLAMADA A LA FUNCION Y LOS VALORES DE a ALMACENADOS. ES RECURSION SIMPLE
 int multiplicacionPorSuma(int a,int b){
 	if(b==0)
 		return 0;
@@ -89,10 +69,8 @@ int potencia(int base,int exponente){
 	return base*potencia(base,exponente-1);
 }
 
-
 int factorial(int numero){
 	if ((numero==0)||(numero==1))
 		return 1;
 	return (numero)*factorial(numero-1);
-
 }
--- src/modulos.h
+++ src/modulos.h
@@ -2,60 +2,67 @@
 #define MODULOS_H_
 
 //SIMPLE OR MULTIPLE RECURSION:
-//PRE:
-//POST:
+//PRE: Recibe un number mayor o igual a cero
+//POST: Devuelve el factorial
 int factorial(int number);
 
+//factorial1 y factorialRecursivoDeCola ambos juntos funcionan como un factorial recursivo pero de cola, no simple(que usa una pila para almacenar valores en varios niveles e ir multiplicando los resultados)
+//es recursividad de cola, parecieria mutua pero no lo es(porque no hacen ping pong con los parametros, solo una vez al principio)
 //RECURSIVIDAD DE COLA:
-//PRE:
-//POST:
+//PRE: Recibe un number mayor o igual a cero
+//POST: Devuelve el factorial previamente calculado con factorialRecursivoDeCola
 int factorial1(int number);
 
-//PRE:
-//POST:
+//PRE: Recibe number y un acumulador
+//POST: Devuelve el resultado a factorial1
 int factorialRecursivoDeCola(int number, int accumulator);
 
 //RECURSIVIDAD UNICA:
-//PRE:
-//POST:
+//PRE: Recibe una base and a exponent validos (exponent >= 0)
+//POST: Devuelve la potencia
 int potencia(int base,int exponent);
 
+//AL IGUAL QUE POTENCIA Y FACTORIAL, VOY ACUMULANDO DATOS EN LA PILA...EL RETURN HACE SUMAS ENTRE LA LLAMADA A LA FUNCION Y LOS VALORES DE a ALMACENADOS. ES RECURSION SIMPLE
 //RECURSIVIDAD UNICA:
-//PRE:
-//POST:
+//PRE: Recibe dos numeros
+//POST: Devuelve su multiplicacion
 int multiplicacionPorSuma(int numberA,int numberB);
 
+//SIEMPRE DEVUELVO (RETURN) LA FUNCION SIN NINGUNA CUENTA ENCIMA...ES RECURSION DE COLA, SIEMPRE TRABAJO SOBRE EL MISMO NIVEL(ES MEJOR)
 //RECURSIVIDAD DE COLA:
-//PRE:
-//POST:
+//PRE: Recibe dos numeros enteros
+//POST: Devuelve el maximo comun divisor de ambos numeros
 int MCD(int numberA, int numberB);
 
+//fibonacci implementado con recurisvidad  multiple, return de 2 veces el llamado de recursion
 //RECURSIVIDAD MULTIPLE:
-//PRE:
-//POST:
+//PRE: Recibe a number natural (el n correspondiente a Fn)
+//POST: Devuelve fibonacci de el number n ingresado
 int fibonacci(int number);
 
 //RECURSIVIDAD MUTUA:
-//PRE:
-//POST:
+//PRE: Recibe un number
+//POST: Devuelve fibo de number
 int fibo (int number);
 
-//PRE:
-//POST:
+//PRE: Recibe un number
+//POST: Devuelve los adultos de ese number
 int adult(int number);
 
-//PRE:
-//POST:
+//PRE: Recibe un number
+//POST: Devuelve los babys de ese number
 int baby(int number);
 
+//RECURSIVIDAD ANIDADA: funcion de ackerman (tarda un montonasooo, crece muy rapido)
 //RECURSIVIDAD ANIDADA: Dentro de los parametros llamo de nuevo a la funcion recursiva
-//PRE:
-//POST:
+//PRE: Recibe two numbers m,n
+//POST: Devuelve el calculo de ackerman de ambos numbers
 int ackermanFunction(int m, int n);
 
+//RECURSIVIDAD MULTIPLE: Dentro de la funcion se lo llama a la funcion recursiva
 //RECURSIVIDAD MULTIPLE: Falta terminar...
-//PRE:
-//POST:
+//PRE:-
+//POST:-
 int towerOfHanoi(int numberOfDiscs);
 
 
